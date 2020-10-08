50790a744459bc220440e331cb34c00f2290da99
dxu5 <derek_xu@brown.edu>
2020-10-07 21:32:57 -0700

    Finish user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,33 @@
 class User < ApplicationRecord
+    attr_reader :password
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+    validates :password_digest, presence: true
+
+    after_initialize :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
+    end
+
+    def password=(password)
+        @password = password
+        self.session_token = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.session_token).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
 end
