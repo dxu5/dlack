5f2cf02347dc64725aa0d0cccb431e4bd45d07bf
dxu5 <derek_xu@brown.edu>
2020-10-07 22:29:46 -0700

    Finish application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+    skip_before_action :verify_authenticity_token
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in
+        redirect_to new_session_url if current_user.nil?
+    end
+
+    def login!(user)
+        @current_user = user
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout
+        current_user.reset_session_token! if !current_user.nil?
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def logged_in?
+        !!current_user
+    end
+    
 end
