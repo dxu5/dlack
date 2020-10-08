d8e0f43933fac3f40ea41028705385b91daf0d21
dxu5 <derek_xu@brown.edu>
2020-10-08 08:18:29 -0700

    Finished creating store and entities users slice of state

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { login, logout, signup } from "./actions/session_actions.js";
+import configureStore from "./store/store.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   let root = document.getElementById("root");
+  let store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.login = login;
   window.logout = logout;
   window.signup = signup;
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,1 +1,8 @@
-import {} from "redux";
+import { combineReducers } from "redux";
+import usersReducer from "./users_reducer.js";
+
+const entitiesReducer = combineReducers({
+  users: usersReducer,
+});
+
+export default entitiesReducer;
