6364836dcae73bf2a21cfd8d5cfbb8f92b545760
dxu5 <derek_xu@brown.edu>
2020-10-07 22:37:08 -0700

    Finish sessions controller and jbuilder views

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,19 @@
 class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user
+            login!(@user)
+            render 'api/users/show'
+        else
+            render json: ["Invalid Username or Password"], status: 404
+        end
+    end
+    def destroy
+        if current_user
+            logout
+            render json: ["You have been logged out"], status: 200
+        else
+            render json: ["There are no users"], status: 404
+        end
+    end
 end
