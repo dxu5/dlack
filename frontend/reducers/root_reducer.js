10a1b412140a62f0ef1972ff707f53562fed835f
dxu5 <derek_xu@brown.edu>
2020-10-08 08:37:40 -0700

    Complete actions, reducers, and store for front end auth

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import entitiesReducer from "./entities_reducer.js";
 import sessionReducer from "./session_reducer.js";
+import errorsReducer from "./errors_reducer.js";
 
 const rootReducer = combineReducers({
   entities: entitiesReducer,
+  errors: errorsReducer,
   session: sessionReducer,
 });
 
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -2,3 +2,16 @@ import {
   RECEIVE_CURRENT_USER,
   RECEIVE_SESSION_ERRORS,
 } from "../actions/session_actions.js";
+
+const sessionErrorsReducer = (state = [], action) => {
+  switch (action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors;
+    case RECEIVE_CURRENT_USER:
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default sessionErrorsReducer;
