b5daebedbbab10dba4c1b9bfe7015326e67f6094
dxu5 <derek_xu@brown.edu>
2020-10-09 08:41:32 -0700

    Finish adding login form container

---
+++ frontend/components/forms/login_form_container.js
@@ -0,0 +1,19 @@
+import { connect } from "react-redux";
+import { login } from "../../actions/session_actions.js";
+import SessionForm from './session_form.jsx'
+
+
+const mapStateToProps = (state) => {
+    return {
+        errors: state.errors.session,
+        formType = "Login"
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        login: (user) => dispatch(login(user))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm)
