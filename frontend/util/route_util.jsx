6c5ae05719581b3cdffc1d31008c999b98638b32
dxu5 <derek_xu@brown.edu>
2020-10-09 10:30:31 -0700

    Finish auth routing logic

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -13,3 +13,27 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => {
     />
   );
 };
+
+const Protected = ({ component: Component, path, loggedIn, exact }) => {
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={(props) => {
+        let final = loggedIn ? <Component {...props} /> : <Redirect to="/" />;
+        return final;
+      }}
+    />
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: Boolean(state.session.currentUserId),
+  };
+};
+
+export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+export const ProtectedRoute = withRouter(
+  connect(mapStateToProps, null)(Protected)
+);
