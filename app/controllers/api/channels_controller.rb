73f6b539920eabe106f95964fc437c79837f181e
dxu5 <derek_xu@brown.edu>
2020-10-12 10:35:06 -0700

    Finish Channel controller show

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -1,7 +1,20 @@
 class Api::ChannelsController < ApplicationController
     def create
+        @channel = Channel.new(channel_params)
+        # Will pass user_ids as a param (string) and will use that!
+        # Channel title if dm will be a string with usernames
+        debugger
+        if @channel.is_dm
+            
+        end
     end
     def show
+        @channel = Channel.find_by(id: params[:id])
+        if @channel
+            render :show #need to create this json jbuilder file!
+        else
+            render json: ["Channel Not Found"], status: 404
+        end
     end
     private
     def channel_params
