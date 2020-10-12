ae1375d900788f5c82ddb07e3c632ddb7ef90ab3
dxu5 <derek_xu@brown.edu>
2020-10-12 13:43:50 -0700

    Finish responsive webpage design

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-
+Channel.destroy_all
+general = Channel.create!(title: "General", is_private: false, is_dm: false)
 demo_user = User.create!(username: "dlack_demo", password: "password")
 user1 = User.create!(username: "dxu5", password: "password")
 user2 = User.create!(username: "dxu6", password: "password")
-# general = Channel.create!(title: "General", is_private: false, is_dm: false)
+
 
