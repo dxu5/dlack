1944212de203bca9b9f220e8b5fd8f2872cc9127
dxu5 <derek_xu@brown.edu>
2020-10-12 14:44:43 -0700

    Finish message controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,4 +1,13 @@
 class Api::MessagesController < ApplicationController
+    def create
+        @message = Message.new(message_params)
+        @message.author_id = current_user.id
+        if @message.save
+            render :show
+        else
+            render json: @message.errors.full_messages, status: 422
+        end
+    end
     private
     def message_params
         params.require(:message).permit(:body, :author_id, :channel_id)
