0e743b2bfbc4867a022c1cea53494ee19d72ff2e
dxu5 <derek_xu@brown.edu>
2020-10-11 21:35:52 -0700

    Finish validations for user channels model

--- app/models/user_channel.rb
+++ app/models/user_channel.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: user_channels
+#
+#  id         :bigint           not null, primary key
+#  user_id    :integer          not null
+#  channel_id :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class UserChannel < ApplicationRecord
+    validates :user_id, :channel_id, presence: true
+    validates :user_id, uniqueness: { scope: :channel_id }
 end
--- test/models/user_channel_test.rb
+++ test/models/user_channel_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: user_channels
+#
+#  id         :bigint           not null, primary key
+#  user_id    :integer          not null
+#  channel_id :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class UserChannelTest < ActiveSupport::TestCase
