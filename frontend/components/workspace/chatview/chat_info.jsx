4c33c4d912f7451cd8ab3f37bd753f35a2f9d026
dxu5 <derek_xu@brown.edu>
2020-10-14 16:35:29 -0700

    Add chat name to chat info

--- frontend/components/workspace/chatview/chat_info.jsx
+++ frontend/components/workspace/chatview/chat_info.jsx
@@ -4,10 +4,21 @@ class ChatInfo extends React.Component {
   constructor(props) {
     super(props);
   }
+  handleTitleType(){
+      if(this.props.currentChannel)
+  }
 
   render() {
-    debugger;
-    return <div className="channel-info-header">hello</div>;
+    return (
+      <div className="channel-info-header">
+        <div className="location-info">
+          <h2 className="channel-info-title">
+
+            {this.props.currentChannel.title}
+          </h2>
+        </div>
+      </div>
+    );
   }
 }
 
