ea51513751fef95546bf2ef7c44a2fb310c3a465
dxu5 <derek_xu@brown.edu>
2020-10-14 16:12:40 -0700

    Create current channel function

--- frontend/components/workspace/chatview/chat_info.jsx
+++ frontend/components/workspace/chatview/chat_info.jsx
@@ -1,1 +1,13 @@
 import React from "react";
+
+class ChatInfo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="channel-info-header">hello</div>;
+  }
+}
+
+export default ChatInfo;
--- frontend/components/workspace/chatview/chat_view.jsx
+++ frontend/components/workspace/chatview/chat_view.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import ChatInfo from "./chat_info.jsx";
 
 class ChatView extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  currentChannel() {
+    let selectedChannel = null;
+    for (const channel in this.props.channels) {
+      if (this.props.match.channelId === channel.id) {
+        selectedChannel = channel;
+      }
+    }
+    return selectedChannel;
+  }
+
   render() {
-    debugger;
-    return <h1>Chat View</h1>;
+    return (
+      <ChatInfo
+        channels={this.props.channels}
+        userChannels={this.props.userChannels}
+        currentChannelId={this.props.match.params.channelId}
+      />
+    );
   }
 }
 
