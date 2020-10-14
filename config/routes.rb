125d9ebbdbafecad71bac4b8c3fb98f37126b06f
dxu5 <derek_xu@brown.edu>
2020-10-14 11:15:57 -0700

    Add destroy channel route to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :index]
     resource :session, only: [:create, :destroy]
-    resources :channels, only: [:create, :show]
+    resources :channels, only: [:create, :show, :destroy]
     resources :messages, only:[:create]
   end
   root to: 'static_pages#root'
