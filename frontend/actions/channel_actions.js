0b7a8bba11a424f6f9fe498149f1fce1b3b1a852
dxu5 <derek_xu@brown.edu>
2020-10-14 10:16:13 -0700

    Create receive channel info action creator

--- frontend/actions/channel_actions.js
+++ frontend/actions/channel_actions.js
@@ -1,11 +1,19 @@
 import * as ChannelAPIUtil from "../util/channel_api_util.js";
 import { receiveErrors } from "./session_actions.js";
 export const RECEIVE_CHANNEL = "RECEIVE_CHANNEL";
+export const RECEIVE_CHANNEL_INFO = "RECEIVE_CHANNEL_INFO";
 
 export const receiveChannel = (channel) => {
   return {
     type: RECEIVE_CHANNEL,
     channel,
+  };
+};
+
+export const receiveChannelInfo = (payload) => {
+  return {
+    type: RECEIVE_CHANNEL_INFO,
+    payload,
   };
 };
 
