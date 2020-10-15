21e7f60cbef1d93b767b18f750440b661e2d6705
dxu5 <derek_xu@brown.edu>
2020-10-15 14:52:28 -0700

    Add place holders to input field

--- frontend/components/workspace/chatview/message_form.jsx
+++ frontend/components/workspace/chatview/message_form.jsx
@@ -3,10 +3,43 @@ import React from "react";
 class MessageForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      body: "",
+      channel_id: Number(this.props.match.params.channelId),
+    };
+  }
+
+  determineType() {
+    if (this.props.currentChannel.is_private) {
+      return (
+        <input
+          type="text"
+          placeholder={`Message 🔒 ${this.props.currentChannel.title}`}
+          style={{ fontFamily: "Poppins, FontAwesome" }}
+          value={this.state.body}
+          className="message-text"
+        />
+      );
+    } else {
+      return (
+        <input
+          type="text"
+          placeholder={`Message  # ${this.props.currentChannel.title}`}
+          value={this.state.body}
+          className="message-text"
+        />
+      );
+    }
   }
 
   render() {
-    return <div>We render message form</div>;
+    return (
+      <div className="channel-message-form">
+        <form className="message-form">
+          <div className="message-input">{this.determineType()}</div>
+        </form>
+      </div>
+    );
   }
 }
 
--- frontend/components/workspace/chatview/message_form_container.js
+++ frontend/components/workspace/chatview/message_form_container.js
@@ -3,10 +3,17 @@ import { withRouter } from "react-router-dom";
 import { createMessage } from "../../../actions/message_actions.js";
 import MessageForm from "./message_form.jsx";
 
+const mapStateToProps = (state, ownProps) => {
+  return {
+    currentChannel: state.entities.channels[ownProps.match.params.channelId],
+  };
+};
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     createMessage: (message) => dispatch(createMessage(message)),
   };
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(MessageForm));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(MessageForm)
+);
