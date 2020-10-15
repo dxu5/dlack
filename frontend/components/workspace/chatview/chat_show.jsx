75d1941e805fc442dea7f77bde6556435770c1df
dxu5 <derek_xu@brown.edu>
2020-10-15 11:01:49 -0700

    Finish basic styling for message container

--- frontend/components/workspace/chatview/chat_show.jsx
+++ frontend/components/workspace/chatview/chat_show.jsx
@@ -7,8 +7,10 @@ class ChatShow extends React.Component {
 
   render() {
     return (
-      <div className="show">
-        <div className="chat-container">Chat Show</div>
+      <div className="show-messages">
+        <div className="chat-container">
+          <div className="message-list-container">Chat View</div>
+        </div>
       </div>
     );
   }
