01dd83c332c45488bdd2984d1e1ba7951bd72763
dxu5 <derek_xu@brown.edu>
2020-10-15 07:02:58 -0700

    Finish updating channels reducer

--- frontend/reducers/channels_reducer.js
+++ frontend/reducers/channels_reducer.js
@@ -1,6 +1,9 @@
 import { RECEIVE_CURRENT_USER } from "../actions/session_actions.js";
-import { RECEIVE_CHANNEL } from "../actions/channel_actions.js";
-import { DELETE_CHANNEL } from "../actions/channel_actions";
+import {
+  DELETE_CHANNEL,
+  RECEIVE_CHANNEL_INFO,
+  RECEIVE_CHANNEL,
+} from "../actions/channel_actions";
 
 const channelsReducer = (state = {}, action) => {
   Object.freeze(state);
@@ -13,6 +16,9 @@ const channelsReducer = (state = {}, action) => {
       return newState;
     case DELETE_CHANNEL:
       delete newState[action.channelId];
+      return newState;
+    case RECEIVE_CHANNEL_INFO:
+      newState[action.payload.channel.id] = action.payload.channel;
       return newState;
     default:
       return state;
