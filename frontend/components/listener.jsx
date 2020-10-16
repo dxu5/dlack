dd78960ec127df5bc748c84f7c5942133258d217
dxu5 <derek_xu@brown.edu>
2020-10-15 23:18:03 -0700

    Finish updating better updating of channels

--- frontend/components/listener.jsx
+++ frontend/components/listener.jsx
@@ -8,16 +8,39 @@ class Listener extends React.Component {
 
   componentDidMount() {
     if (this.props.currentUser) {
-      this.createSockets();
+      this.createSockets(this.props.channelIds);
     }
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps, prevState) {
+    //CHECK WITH RYAN IF THIS IS OK???????
     if (
       this.props.currentUser &&
       this.props.currentUser !== prevProps.currentUser
     ) {
-      this.createSockets();
+      this.createSockets(this.props.channelIds);
+    } else if (
+      this.chats &&
+      prevProps.channelIds.length !== this.props.channelIds.length
+    ) {
+      for (let i = 0; i < this.chats.length; i++) {
+        let channel = JSON.parse(this.chats[i].identifier);
+        if (!this.props.channelIds.includes(channel.channel_id)) {
+          this.chats[i].unsubscribe();
+        }
+      }
+      let oldChannelIds = this.chats.map((chat) => {
+        return JSON.parse(chat.identifier).channelId;
+      });
+      let final = [];
+      for (let i = 0; i < this.props.channelIds.length; i++) {
+        if (!oldChannelIds.includes(this.props.channelIds[i])) {
+          final.push(this.props.channelIds[i]);
+        }
+      }
+      if (final.length > 0) {
+        this.createSockets(final);
+      }
     }
   }
 
@@ -28,12 +51,10 @@ class Listener extends React.Component {
     }
   }
 
-  createSockets() {
+  createSockets(channelIds) {
     console.log("Running create socket");
 
-    let channelIds = this.props.channelIds;
-
-    this.chats = channelIds.map((id) => {
+    let result = channelIds.map((id) => {
       return App.cable.subscriptions.create(
         {
           channel: "MessageChannel",
@@ -56,6 +77,11 @@ class Listener extends React.Component {
         }
       );
     });
+    if (this.chats === undefined) {
+      this.chats = result;
+    } else {
+      this.chats = this.chats.concat(result);
+    }
   }
 
   render() {
