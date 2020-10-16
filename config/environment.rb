c208b6129c1f1dc80ee238a3637c1d329c2929dc
dxu5 <derek_xu@brown.edu>
2020-10-16 10:07:08 -0700

    Configure active cable

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+
+
+config.web_socket_server_url = "wss://dlack.herokuapp.com/cable" 
+config.action_cable.allowed_request_origins = ['https://dlack.herokuapp.com', 'http://dlack.herokuapp.com']
