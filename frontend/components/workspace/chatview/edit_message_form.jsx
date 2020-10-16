c78cfc9bdf01796070a1645d9d144c33e5ec8ed1
dxu5 <derek_xu@brown.edu>
2020-10-16 16:27:48 -0700

    Got barebones of editing worked out

--- frontend/components/workspace/chatview/edit_message_form.jsx
+++ frontend/components/workspace/chatview/edit_message_form.jsx
@@ -1,13 +1,47 @@
 import React from "react";
-
+import { connect } from "react-redux";
+import { updateMessage } from "../../../actions/message_actions";
 class EditMessageForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = this.props.message;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ body: e.currentTarget.value });
+  }
+
+  handleSubmit() {
+    this.props.updateMessage(this.state).then(this.props.handleClose());
   }
 
   render() {
-    return <button onClick={this.props.handleClose}>editing</button>;
+    return (
+      <div className="message-edit">
+        <div className="edit-form-container">
+          <form>
+            <div className="input">
+              <input
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.body}
+              />
+              <button onClick={this.props.handleClose}>Cancel</button>
+              <button onClick={this.handleSubmit}>Edit Message</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
-export default EditMessageForm;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateMessage: (message) => dispatch(updateMessage(message)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(EditMessageForm);
