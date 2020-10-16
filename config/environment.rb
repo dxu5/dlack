652a1f266fc7f4bc8650f874f6082bc25f06825e
dxu5 <derek_xu@brown.edu>
2020-10-16 10:09:51 -0700

    made changes to production.rb and environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
-
-
-config.web_socket_server_url = "wss://dlack.herokuapp.com/cable" 
-config.action_cable.allowed_request_origins = ['https://dlack.herokuapp.com', 'http://dlack.herokuapp.com']
