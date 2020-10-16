caa0f045ff018c7414ae2540f17c018f605529ae
dxu5 <derek_xu@brown.edu>
2020-10-16 11:21:01 -0700

    Finish adding timestamp to messages

--- frontend/components/workspace/chatview/message_index_item.jsx
+++ frontend/components/workspace/chatview/message_index_item.jsx
@@ -10,7 +10,10 @@ class MessageIndexItem extends React.Component {
       <li className="user-message">
         <img className="user-profile" src={window.images.user} />
         <div className="message-details">
-          <h2 className="message-username">{this.props.user.username}</h2>
+          <h2 className="message-username">
+            {this.props.user.username}
+            <span className="timestamp">{this.props.message.updated_at}</span>
+          </h2>
           <p>{this.props.message.body}</p>
         </div>
       </li>
