affced9215b1c6500821f0fed5e13564b6247b87
dxu5 <derek_xu@brown.edu>
2020-10-18 15:46:41 -0700

    Add update route to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :index]
+    resources :users, only: [:create, :index, :update]
     resource :session, only: [:create, :destroy]
     resources :channels, only: [:create, :show, :destroy, :update]
     resources :messages, only:[:create, :destroy, :update]
