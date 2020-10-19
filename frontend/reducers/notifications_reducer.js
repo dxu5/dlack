a2e023a31bdcb3fbc778f4d22ddb68f0daa9c3aa
dxu5 <derek_xu@brown.edu>
2020-10-19 11:38:29 -0700

    Complete notification logic for erasing notifications

--- frontend/reducers/notifications_reducer.js
+++ frontend/reducers/notifications_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_CURRENT_USER } from "../actions/session_actions.js";
+import { RECEIVE_CHANNEL_INFO } from "../actions/channel_actions";
 
 const notificationsReducer = (state = {}, action) => {
   Object.freeze(state);
@@ -6,6 +7,11 @@ const notificationsReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       return action.notifications;
+    case RECEIVE_CHANNEL_INFO:
+      Object.values(action.payload.notifications).forEach((notification) => {
+        newState[notification.id] = notification;
+      });
+      return newState;
     default:
       return state;
   }
