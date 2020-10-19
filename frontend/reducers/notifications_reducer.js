e2cb00fc4ac5f20d225fecf3e5397b9a17b2a7e4
dxu5 <derek_xu@brown.edu>
2020-10-19 11:52:17 -0700

    Fix error on notifications reducer when creating a channel

--- frontend/reducers/notifications_reducer.js
+++ frontend/reducers/notifications_reducer.js
@@ -1,6 +1,6 @@
 import { RECEIVE_CURRENT_USER } from "../actions/session_actions.js";
 import { RECEIVE_CHANNEL_INFO } from "../actions/channel_actions";
-
+import { RECEIVE_MESSAGE } from "../actions/message_actions.js";
 const notificationsReducer = (state = {}, action) => {
   Object.freeze(state);
   let newState = Object.assign({}, state);
@@ -8,9 +8,16 @@ const notificationsReducer = (state = {}, action) => {
     case RECEIVE_CURRENT_USER:
       return action.notifications;
     case RECEIVE_CHANNEL_INFO:
-      Object.values(action.payload.notifications).forEach((notification) => {
-        newState[notification.id] = notification;
-      });
+      if (action.payload.notifications) {
+        Object.values(action.payload.notifications).forEach((notification) => {
+          newState[notification.id] = notification;
+        });
+        return newState;
+      } else {
+        return newState;
+      }
+    case RECEIVE_MESSAGE:
+      newState[action.payload.notification.id] = action.payload.notification;
       return newState;
     default:
       return state;
