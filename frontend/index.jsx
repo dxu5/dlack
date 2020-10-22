c0994fc19d4b0ea18776c9cdc1766ead22a92f49
dxu5 <derek_xu@brown.edu>
2020-10-22 15:25:46 -0700

    Clean index.jsx

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { login, logout, signup } from "./actions/session_actions.js";
-import { searchUsers } from "./actions/search_actions.js";
-import { getChannelInfo } from "./actions/channel_actions.js";
 import configureStore from "./store/store.js";
 import Root from "./components/root.jsx";
-import { createMessage, updateMessage } from "./actions/message_actions";
-import { updateUser } from "./actions/user_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   let root = document.getElementById("root");
@@ -27,9 +22,5 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore();
   }
-  window.getChannelInfo = getChannelInfo;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.updateUser = updateUser;
   ReactDOM.render(<Root store={store} />, root);
 });
